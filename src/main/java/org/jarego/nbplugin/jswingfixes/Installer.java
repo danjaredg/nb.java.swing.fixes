b99b84ac8e47f48fcec20fd5f89c8d7f0df2cb9c
Jared González <danjaredg@gmail.com>
2018-03-11 14:13:55 -0600

    Invocación segura para modificadores java swing

--- src/main/java/org/jarego/nbplugin/jswingfixes/Installer.java
+++ src/main/java/org/jarego/nbplugin/jswingfixes/Installer.java
@@ -1,10 +1,16 @@
 package org.jarego.nbplugin.jswingfixes;
 
+import javax.swing.SwingUtilities;
 import org.openide.modules.ModuleInstall;
 
 public class Installer extends ModuleInstall {
 	@Override
 	public void restored() {
-		ExtendedLookAndFeel.applyLookAndFeelExtended();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				ExtendedLookAndFeel.applyLookAndFeelExtended();
+			}
+		});
 	}
 }
